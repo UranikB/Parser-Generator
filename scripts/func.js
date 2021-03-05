95830a492a5169af10627b379ae4c11e35c5dc17
Uranik Berisha <uranik.berisha@mni.thm.de>
2021-03-05 16:38:48 +0100

    Added input fields

---
+++ scripts/func.js
@@ -0,0 +1,25 @@
+function addFields(){
+    const container = document.getElementById("input-form");
+
+    const inputField = document.createElement("div");
+    inputField.classList.add("input-field");
+
+    const input1 = document.createElement("input");
+    input1.classList.add("nonterminal");
+    input1.maxLength = 1;
+    input1.type = "text";
+
+    const arrow = document.createElement("span");
+    arrow.innerHTML = "&#8594;"
+
+
+    const input2 = document.createElement("input");
+    input2.classList.add("production-rule");
+    input2.type = "text";
+
+    inputField.appendChild(input1);
+    inputField.appendChild(arrow);
+    inputField.appendChild(input2);
+
+    container.insertBefore(inputField, document.getElementById("add-input-field"));
+}
