95b3467fdcb50c04e7ea700a1df931ae22c581e5
Uranik Berisha <uranik.berisha@mni.thm.de>
2021-03-06 14:20:33 +0100

    added delete buttons

--- scripts/func.js
+++ scripts/func.js
@@ -1,4 +1,4 @@
-function addFields(){
+function addField(){
     const container = document.getElementById("input-form");
 
     const inputField = document.createElement("div");
@@ -10,16 +10,22 @@ function addFields(){
     input1.type = "text";
 
     const arrow = document.createElement("span");
-    arrow.innerHTML = "&#8594;"
+    arrow.innerHTML = "&#8594;";
 
 
     const input2 = document.createElement("input");
     input2.classList.add("production-rule");
     input2.type = "text";
 
+    inputField.innerHTML = "<button class=\"delete-field-button\" onclick=\"deleteField(this)\">&#128465;</button>";
     inputField.appendChild(input1);
     inputField.appendChild(arrow);
     inputField.appendChild(input2);
 
-    container.insertBefore(inputField, document.getElementById("add-input-field"));
+    container.insertBefore(inputField, document.getElementById("add-field-button"));
+}
+
+function deleteField(button){
+    let field = button.parentNode;
+    field.parentElement.removeChild(field)
 }
