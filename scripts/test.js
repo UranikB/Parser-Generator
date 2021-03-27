1a81bac15b807b1b9e66c5e7ffe8d1dd5d04a120
thomas <thomas.moissl@mni.thm.de>
2021-03-27 16:36:34 +0100

    Fixed initialzeValuesForTesting function

--- scripts/test.js
+++ scripts/test.js
@@ -2,13 +2,11 @@ function initializeValuesForTesting(){
     const container = document.getElementById("input-form");
     let productionRules = getTestProductionRules();
     let counter = 0;
-    for (let i = 0; i < Object.keys(productionRules).length - 1; i++) {
-        for (let j = 0; j < productionRules[Object.keys(productionRules)[i]].length; j++) {
-            addField();
-        }
-    }
     for (let i = 0; i < Object.keys(productionRules).length; i++) {
         for (let j = 0; j < productionRules[Object.keys(productionRules)[i]].length; j++) {
+            if(container.getElementsByClassName("nonterminal").length <= counter){
+                addField();
+            }
             container.getElementsByClassName("nonterminal")[counter].value = Object.keys(productionRules)[i];
             container.getElementsByClassName("production-rule")[counter].value = productionRules[Object.keys(productionRules)[i]][j];
             counter++;
